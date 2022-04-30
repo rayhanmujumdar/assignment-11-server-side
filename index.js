b42a81c98b8b48a8d6a69735b8d554c8677ea335
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-04-30 17:03:16 +0600

    mongobd setup server site

--- index.js
+++ index.js
@@ -6,7 +6,28 @@ require('dotenv').config()
 
 // middleware
 app.use(cors())
-app.use(express())
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ncyrv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async () => {
+    try{
+        await client.connect()
+        const productCollection = client.db('product').collection('order')
+        app.post('/order', async(req,res) => {
+            const posts = req.body
+            const result = await productCollection.insertOne(posts)
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
 
 app.get('/',(req,res) => {
     res.send('all ok');
