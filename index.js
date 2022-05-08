d9340301c3fa6c8eb14057a91098f64877b53986
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-05-08 23:26:59 +0600

    git ignore not working

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors")
 require('dotenv').config()
 var jwt = require('jsonwebtoken');
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // heroku api link
 // https://fathomless-earth-22258.herokuapp.com
@@ -29,7 +30,6 @@ const verifyToken = (req,res,next) => {
 }
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ncyrv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const run = async () => {
